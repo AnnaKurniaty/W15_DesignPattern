7980f5224ba6c7b544742309f7cd6d1e3090b4f2
xixixixi <77472231+lATHENEvender@users.noreply.github.com>
2021-07-23 16:28:04 +0700

    Add files via upload

---
+++ Singleton Pattern/src/DataManager.java
@@ -0,0 +1,17 @@
+
+public class DataManager {
+    private static DataManager dataManager;
+
+    public static DataManager getInstance() {
+        if (dataManager == null) {
+            dataManager = new DataManager();
+        }
+        return dataManager;
+    }
+
+    private DataManager() { }
+    
+    public void showMessage(){
+        System.out.println("Hello World!");
+     }
+}
---
+++ Singleton Pattern/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args){
+    	System.out.println(DataManager.getInstance());
+        System.out.println(DataManagerMultiThreadingSafe.getInstance());
+        
+    }
+}
